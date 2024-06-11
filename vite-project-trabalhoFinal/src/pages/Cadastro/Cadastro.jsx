3beb88c45cd95471f63d1e914b9def1b212506d1
Lucas de Araújo Travassos <162334444+LucasTravassos22@users.noreply.github.com>
2024-06-11 01:44:54 -0300

    Atualização do estilo da página de cadastro

--- vite-project-trabalhoFinal/src/pages/Cadastro/Cadastro.jsx
+++ vite-project-trabalhoFinal/src/pages/Cadastro/Cadastro.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { postCliente } from "../../services/usarioService/usuarioService";
 import styles from "../Cadastro/Cadastro.module.css";
-import cadastro from "../../images/cadastro.png"
+import Cadastro_icon from "../../images/cadastro_icon.png";
 
 export function Cadastro() {
   const navigate = useNavigate();
@@ -27,42 +27,24 @@ export function Cadastro() {
   }
 
   return(
-    <main>
-      <div>
-        
-          <div className={styles.marrom}>
-
-          <h1>Cadastre-se</h1>
-        {/* <img height="200px" src={cadastro.png}></img> */}
-          
-
-        <form>
-         
-       <input name = "nome" placeholder="Digite seu nome aqui" value={formInfo.nome} onChange={adicionar}></input>
-       <br />
-      
-       <input name = "telefone" placeholder="Digite seu telefone aqui"value={formInfo.telefone} onChange={adicionar}></input>
-        <br />
-     
-       <input name = "email" placeholder="Digite seu email aqui"value={formInfo.email} onChange={adicionar}></input>
-        <br />
-        
-       <input name = "cpf" placeholder="Digite seu cpf aqui" value={formInfo.cpf} onChange={adicionar}></input>
-        <br />
-        
-       <input name = "cep" placeholder="Digite seu cep aqui"value={formInfo.cep} onChange={adicionar}></input>
-        <br />
-        </form>
-     
-       <input name = "senha" type="password" placeholder="Digite sua senha aqui" value={formInfo.senha} onChange={adicionar}></input>
-        <br />
+    <div className={styles.cadastro}>
+      <img className="icon" src={Cadastro_icon}></img>
+      <form className={styles.form}>
+        <input className={styles.imput} name = "nome" placeholder="Digite seu nome aqui" value={formInfo.nome} onChange={adicionar}></input>
+          <br />
+        <input className={styles.imput2} name = "telefone" placeholder="Digite seu telefone aqui"value={formInfo.telefone} onChange={adicionar}></input>
+          <br />
+        <input className={styles.imput3} name = "email" placeholder="Digite seu email aqui"value={formInfo.email} onChange={adicionar}></input>
+          <br />   
+        <input className={styles.imput4} name = "cpf" placeholder="Digite seu cpf aqui" value={formInfo.cpf} onChange={adicionar}></input>
+          <br />
+        <input className={styles.imput5} name = "cep" placeholder="Digite seu cep aqui"value={formInfo.cep} onChange={adicionar}></input>
+          <br />
+        <input className={styles.imput6} name = "senha" type="password" placeholder="Digite sua senha aqui" value={formInfo.senha} onChange={adicionar}></input>
+          <br />
         <button className={styles.button1} type="button" onClick={()=> criarCliente(formInfo)}>Enviar</button>
-        </div>
-      </div>
-    </main>
-  
-
-
+      </form>
+    </div>
   )
 }
 
